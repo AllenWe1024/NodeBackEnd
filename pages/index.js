ca80ab2abaed842599abff08c4a367cee06658fd
ZhuoranTian <849597043@qq.com>
2022-02-13 07:19:37 +0000

    切图

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 const port = 80
 
-app.use(express.static('public'))
+app.use(express.static('public',{index:"login.html"}))
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/login.html");
-})
+// console.log(__dirname+"/public/login.html");
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + "/public/login.html");
+// })
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
