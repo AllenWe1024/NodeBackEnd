f7444df249f594756b5384b06951659d260eae6c
ZhuoranTian <849597043@qq.com>
2022-02-13 17:06:59 +0800

    反向代理

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,41 @@
-const express = require('express')
-const app = express()
-const port = 80
+// Express反向代理
+const express = require('express');
+const timeout = require('connect-timeout');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const app = express();
 
-app.use(express.static('public',{index:"login.html"}))
+// HOST 指目标地址 PORT 服务端口
+const HOST = 'http://localhost', PORT = '10002';
 
-// console.log(__dirname+"/public/login.html");
+// 超时时间
+const TIME_OUT = 30 * 1e3;
 
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + "/public/login.html");
-// })
+// 设置端口
+app.set('port', PORT);
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
-})
+// 设置超时 返回超时响应
+app.use(timeout(TIME_OUT));
+app.use((req, res, next) => {
+  if (!req.timedout) next();
+});
+
+// 设置静态资源路径
+app.use(express.static('public',{index:"login.html"}));
+
+// 反向代理（这里把需要进行反代的路径配置到这里即可）
+// eg:将/api 代理到 ${HOST}/api
+// app.use(createProxyMiddleware('/api', { target: HOST }));
+// 自定义代理规则
+app.use(createProxyMiddleware('/', {
+  target: HOST, // target host
+  changeOrigin: true, // needed for virtual hosted sites
+  ws: true, // proxy websockets
+  // pathRewrite: {
+  //   '^/': '', // rewrite path
+  // }
+}));
+
+// 监听端口
+app.listen(app.get('port'), () => {
+  console.log(`server running ${HOST}:${PORT }`);
+});
