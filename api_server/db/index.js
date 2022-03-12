3c40b142adcd837e0ca1e8590a5c9247976835e5
ZhuoranTian <849597043@qq.com>
2022-03-12 21:15:28 +0800

    update MySQL

--- api_server/db/index.js
+++ api_server/db/index.js
@@ -5,7 +5,7 @@ const mysql = require('mysql')
 const db = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
-  password: '123456',
+  password: 'frsgwstrGYIUeg123134!!!',
   database: 'my_db_01',
 })
 
